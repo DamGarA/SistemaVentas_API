4db53fff326db07218265fa07b5ffc1e6e59f70c
DamGarA <damiangarciaabreu@gmail.com>
2023-07-14 23:35:20 -0300

    first push

---
+++ SistemaVenta.DAL/Repositorios/VentaRepository.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SistemaVenta.DAL.DBContext;
+using SistemaVenta.DAL.Repositorios.Contrato;
+using SistemaVenta.MODEL;
+
+namespace SistemaVenta.DAL.Repositorios
+{
+    public class VentaRepository : GenericRepository<Venta>, IVentaRepository
+    {
+        private readonly DbventaContext _dbContext;
+
+        public VentaRepository(DbventaContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Venta> Registrar(Venta modelo)
+        {
+            Venta ventaGenerada = new Venta();
+
+            using (var transaccion = _dbContext.Database.BeginTransaction()) 
+            {
+                try
+                {
+                    //cómo funciona exactamente este modelo.DetalleVenta? 
+                    foreach (DetalleVenta dv in modelo.DetalleVenta)
+                    {
+                        Producto producto_encontrado = _dbContext.Productos
+                            .Where(p => p.IdProducto == dv.IdProducto).First();
+
+                        producto_encontrado.Stock = producto_encontrado.Stock - dv.Cantidad;
+
+                        _dbContext.Productos.Update(producto_encontrado);
+                    }
+                    await _dbContext.SaveChangesAsync();
+
+                    NumeroDocumento correlativo = _dbContext.NumeroDocumentos.First();
+
+                    correlativo.UltimoNumero = correlativo.UltimoNumero + 1;
+                    correlativo.FechaRegistro = DateTime.Now;
+
+                    _dbContext.NumeroDocumentos.Update(correlativo);
+                    await _dbContext.SaveChangesAsync();
+
+                    int CantidadDigitos = 4;
+                    string ceros = string.Concat(Enumerable.Repeat("0", CantidadDigitos));
+                    string numeroVenta = ceros + correlativo.UltimoNumero.ToString();
+                    numeroVenta = numeroVenta.Substring(numeroVenta.Length - CantidadDigitos, CantidadDigitos);
+                    //por que lo corta de esa manera?
+
+                    modelo.NumeroDocumento = numeroVenta;
+
+                    await _dbContext.Venta.AddAsync(modelo);
+                    await _dbContext.SaveChangesAsync();
+
+                    ventaGenerada = modelo;
+
+                    transaccion.Commit();
+                }
+                catch
+                {
+                    transaccion.Rollback();
+                    throw;
+                }
+                return ventaGenerada;
+            }
+            //return ventaGenerada; ----> puede ir aca afuera del using?
+        }
+    }
+}
